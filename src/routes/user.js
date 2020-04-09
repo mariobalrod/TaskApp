9baf204c19807a570f1336779c12e7def37113e1
mariobalrod <marioballestero.rdg@gmail.com>
2020-04-09 19:58:38 +0200

    Añadiendo modelo del Usuario, funcionalidad de registro de usuarios y configurando/usando el modulo connect-flash para mensajes de error o registro

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,60 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 router.get('/user/signin', (req, res) => {
     res.render('user/signIn');
 });
 
 router.get('/user/signup', (req, res) => {
     res.render('user/signUp');
+});
+
+router.post('/user/signup', async (req, res) => {
+
+    const { email, username, password, confirmPassword } = req.body;
+    const errors = [];
+    if(password!=confirmPassword){
+        errors.push({text: 'Las contraseñas no coinciden'});
+    }
+    if(password.length < 5){
+        errors.push({text: 'La contraseña es demasiado corta.'});
+    }
+
+    if(errors.length > 0){
+
+        res.render('user/signUp', {errors, email, username, password, confirmPassword});
+
+    }else {
+
+        // TODO Comprobando si las credenciales ya estan en uso
+        const emailUser = await User.findOne({email: email});
+        const usernameUser = await User.findOne({username: username});
+        if(emailUser) {
+            req.flash('error_msg', 'Email is already in use');
+            res.redirect('/user/signup');
+        } else if (usernameUser) {
+            req.flash('error_msg', 'Username is already in use');
+            res.redirect('/user/signup');
+        } else {
+            // TODO Guardando usuario en base de datos
+            const newUser = new User({username, email, password});
+            //? Encrypted password
+            newUser.password = await newUser.encryptPassword(password);
+
+            await newUser.save();
+
+            //? Mensaje de registrado usando la dependencia connect-flash
+            req.flash('success_msg', 'You are registered!');
+
+            console.log('New User Registered!');
+
+            res.redirect('/user/signIn');
+        }
+        
+    }
+
 });
 
 router.get('/user/profile', (req, res) => {
